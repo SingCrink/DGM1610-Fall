bfb4d38f38ab8d0d3f4c682f0dd9f067ef63bb1f
PaigeG <10885052@my.uvu.edu>
2019-10-02 17:20:36 -0600

    projectile Mechanics
    added the projectile features to the animals and the fish

--- prototype 2/Assets/scripts/controller.cs
+++ prototype 2/Assets/scripts/controller.cs
@@ -8,6 +8,7 @@ public class controller : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectileprefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +28,12 @@ public class controller : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            Instantiate(projectileprefab, transform.position, projectileprefab.transform.rotation);
+
+            //launch food at animals
+        }
     }
 }
---
+++ prototype 2/Assets/scripts/moveforward.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveforward : MonoBehaviour
+{
+    public float speed = 40f;
+    public GameObject projectileprefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
