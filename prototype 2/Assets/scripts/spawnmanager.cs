a9a5d3b9a6d3483e15d1e0a239481ab972331061
PaigeG <10885052@my.uvu.edu>
2019-10-07 17:12:54 -0600

    Randomized animals spawn points
    made animals spawn in new places when button is pressed.

---
+++ prototype 2/Assets/scripts/spawnmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            // randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
+            int animalIndex = Random.Range(0,animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation); }}
+            
+}
