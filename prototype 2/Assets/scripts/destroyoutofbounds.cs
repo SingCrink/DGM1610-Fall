9849aa32841d99cd1e4b3338418a628e53d37aed
PaigeG <10885052@my.uvu.edu>
2019-10-09 18:12:07 -0600

    Need to fix debugging issues
    cant figure out how to make the game work fully when its supposed to be finished. Will come back to later.

--- prototype 2/Assets/scripts/destroyoutofbounds.cs
+++ prototype 2/Assets/scripts/destroyoutofbounds.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class destroyoutofbounds : MonoBehaviour
 {
-    private float topBound = 30f;
+    private float topBound = 30;
+    private float lowerBound = -10;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +16,14 @@ public class destroyoutofbounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound){
+        if(transform.position.z > topBound)
+        {
             Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
 
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
         }
         
     }
