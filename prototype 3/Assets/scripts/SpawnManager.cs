d0e2400508f0e97931220bb5818d9364900584d2
PaigeG <10885052@my.uvu.edu>
2019-10-21 17:05:43 -0600

    finished Prototype 3
    need to fix some errors. Will do tomorrow

--- prototype 3/Assets/scripts/SpawnManager.cs
+++ prototype 3/Assets/scripts/SpawnManager.cs
@@ -16,8 +16,8 @@ private float repeatRate = 2;
 
     // Update is called once per frame
     void Update()
-    void SpawnObstacle ()
     {
+        void SpawnObstacle ();
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
---
+++ prototype 3/Assets/scripts/moveleft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveleft : MonoBehaviour
+{
+    private float speed = 30;
+    private float leftBound = -15;
+    private PlayerController PlayerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerControllerScript.gameOver == false){
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.x <leftBound && GameObject.CompareTag("Obstacle")){
+            Destroy(GameObject);
+        }
+    }
+}
