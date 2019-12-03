374610721a62c81c177360c7838927d78434413b
Paige <10885052@my.uvu.edu>
2019-12-03 02:50:10 -0700

    Ball moves with camera
    made ball move with camera and ball moves forward and back with up & down arrow keys

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
